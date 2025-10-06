025459dbe748d794b3fed9d88e1e6878c0873d1f
Basem Serag <basemserag.911@gmail.com>
2025-10-06 12:20:56 +0100

    feat(workspace): add angular-lab & clinic apps with dared libs

---
+++ apps/angular-lab/app/features/signals/signals.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-signals',
+  imports: [],
+  templateUrl: './signals.html',
+  styleUrl: './signals.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class Signals {}
