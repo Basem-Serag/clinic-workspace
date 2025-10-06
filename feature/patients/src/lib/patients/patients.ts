b62c34906b9f97e731edd40246d852c79f4b450f
Basem Serag <basemserag.911@gmail.com>
2025-10-06 12:12:47 +0100

    chore: bootstrap Nx workspace

---
+++ feature/patients/src/lib/patients/patients.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-patients',
+  imports: [],
+  templateUrl: './patients.html',
+  styleUrl: './patients.css',
+})
+export class Patients {}
